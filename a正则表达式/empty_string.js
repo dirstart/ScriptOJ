f790e69e9d5cb9b6d1dde713e84485e3bf3980be
zhuqianye <q835041058@gmail.com>
2017-10-14 16:02:19 +0800

    my-trim end

--- a正则表达式/empty_string.js
+++ a正则表达式/empty_string.js
@@ -1,12 +1,9 @@
-let a="This is our world.";
-let b="If I'm a hero,don't ask me where I'm from.If I'm a jerk,don't ask me the age."
-// my first
-
+let a="  This is our world.";
+// 1.my first
 String.prototype.show=function(){
 	console.log(this);
 	return this;
 }
-
 String.prototype.trim=function(){
 	let res=[];
 	let old=this.split('');
@@ -18,5 +15,32 @@ String.prototype.trim=function(){
 	res=res.join('');
 	return res;
 }
-a.show();
-console.log(a.trim());
+// a.show();
+// console.log(a.trim());
+
+// 2.my second ，在数组操作上的优化
+const trim2=(str)=>{
+	let res=[];
+	let old=str.split('');
+	res=old.filter(item=>item!==' ').join('');
+	return res;
+}
+// console.log(trim2(a));
+
+// 3.reg third，为了体现正则的好像，可以对比一下前面的
+//   有话好好说，为什么要把我从字符串变成数组操作？
+const trim_of_reg=(str)=>{
+	return str.replace(/\s+/g,"");
+}
+// console.log(trim_of_reg(a));
+
+// 4.只去除两边的空格
+const trim_reg2=(str)=>{
+	return str.replace(/^\s*|\s*$/,"");
+}
+console.log(trim_reg2(a));
+// 5.只去除尾空格
+const trim_reg3=(str)=>{
+	return str.replace(/\s*$/,'');
+}
+console.log(trim_reg3(a));
