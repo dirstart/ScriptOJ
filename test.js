eb02d4ec615aff33efc6a77303e1395787fadbc9
zhuqianye <q835041058@gmail.com>
2017-12-18 20:56:50 +0800

    clear

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-let a="h h";
-
-console.log(" "==a[1]," "===a[1]);
+let array=[1,2,3,5,7,8,11,21,33];
+function ran(){
+	return 0.5 - Math.random();
+}
+array.sort(ran());
+console.log(array);   //  [1,11,2,21,3,33,5,7,8]
