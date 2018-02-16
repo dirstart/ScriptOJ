5ddedcb8d71987599e0ed91092c8cd1f0a5c4315
dirstart <q835041058@gmail.com>
2018-02-16 16:30:12 +0800

    array unique

--- NewClassic/A/6_数组去重.js
+++ NewClassic/A/6_数组去重.js
@@ -1,3 +1,35 @@
 // JS语言精粹 - 第三章 - 检索
 // 要检索对象里包含的值，可以采用在 [] 后缀中括住一个字符串表达式
 
+const one = [8, 3, 9, 101, 88, 8, 9, 33, 33];
+
+const unique = (arr) => {
+	let obj = {};
+	let res = [];
+	for (const i of arr) {
+		if (!obj[i]) {
+			obj[i] = true;
+			res.push(i);
+		}
+	}
+	return res;
+}
+console.log(unique(one));
+
+const two = [8, 3, 9, 101, 88, '8', 9, '33', 33];
+// 此处只有 9 应该被去除一个
+
+const unique2 = (arr) => {
+	let obj = {};
+	let res = [];
+	for (const i of two) {
+		const type = typeof i;
+		const name = type + i;
+		if (!obj[name]) {
+			obj[name] = i;
+			res.push(i);
+		}
+	}
+	return res;
+}
+console.log(unique2(two));
