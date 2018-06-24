108cee89dd4b765a6d69a102037372d6b0dd49e1
dirstart <835041058@qq.com>
2018-06-24 17:42:35 +0800

    float problem review

--- test.js
+++ test.js
@@ -1,17 +1,3 @@
-
-let num = 0;
-
-// for (let i = 0; i < 1000000000; i++) {
-//   ++num;
-// };
-
-// console.log('最后的数字是', num);
-
-
-setTimeout(() => {
-  for (let i = 0; i < 1000000; i++) {
-    ++num;
-  }
-}, 0);
-
-console.log('最后的数字是', num);
+const array = [NaN, NaN, ,];
+console.log(array.includes(undefined));
+console.log(array.indexOf(undefined));
