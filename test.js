e7710504c9a1d6573d1273340d12ca771eb4229f
dirstart <835041058@qq.com>
2018-09-19 00:23:38 +0800

    模拟工作环境

--- test.js
+++ test.js
@@ -1,3 +1,41 @@
-const array = [NaN, NaN, ,];
-console.log(array.includes(undefined));
-console.log(array.indexOf(undefined));
+// 实现返回一个斐波那契函数
+
+// let arr = [];
+// (function feibo (num) {
+//   if (num === 1 || num === 2) {
+//     if (arr.length < 2) {
+//       arr.push(1);
+//     }
+//     return 1;
+//   }
+//   var result = arguments.callee(num - 1) + arguments.callee(num - 2);
+//   if (result > arr[arr.length - 1]) {
+//     arr.push(result);
+//   }
+//   return result;
+// })(10);
+
+// console.log(arr);
+
+// [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+let arr = [];
+function methodTwo (num) {
+  // return ((num) => {
+    if ((num === 1 || num === 2)) {
+      if (arr.length < 2) {
+        arr.push(1);
+      }
+      return 1;
+    }
+    var result = arguments.callee(num - 1) + arguments.callee(num - 2);
+    if (result > arr[arr.length - 1]) {
+      arr.push(result);
+    }
+    return result;
+  // })(num);
+}
+
+methodTwo(3);
+console.log(arr);
+
